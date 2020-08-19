cbd023fc4c0e6e2c33eb390b1f95308cc928c060
Reagan Barrington <reagan@barringtonfamily.net>
2020-08-19 15:30:25 -0400

    day3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,7 +10,7 @@ class CPU:
         # holds 256 bytes of memory
         self.ram = [0] * 256
         #  and 8 general-purpose registers
-        self.reg = [0, 0, 0, 0, 0, 0, 0, 0xf4]
+        self.reg = [0, 0, 0, 0, 0, 0, 0, 0]
         self.pc = 0 
 
     def ram_read(self, address):
@@ -26,24 +26,11 @@ class CPU:
         address == Memory Data Register - data that was read or the data to write.
         value == Memory Address Register - address that is being read or written to.
         """
-        self.ram[value] = address
+        self.ram[address] = value
 
 
     def load(self, fileName):
         """Load a program into memory."""
-
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
         try: 
             address = 0
 
@@ -51,9 +38,12 @@ class CPU:
 
                 for line in program:
                     instruction = line.split('#')[0].strip()
+                    # ignore blank lines
                     if instruction != '':
+                        # int() converts binary strings to integer values with number base as second argument
                         self.ram[address] = int(instruction, 2)
                         address += 1
+
         except FileNotFoundError:
             print('File not found')
             sys.exit(2)
@@ -64,7 +54,8 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -92,10 +83,17 @@ class CPU:
         """Run the CPU."""
         halted = False
 
+        # Stack Pointer (R7) as per specs
+        SP = 7
+        self.reg[SP] = 0xf4
+
         # Operations we can perform:
-        HLT = 1
+        HLT = 0b00000001
         LDI = 0b10000010 # Save a value in a register
         PRN = 0b01000111
+        MUL = 0b10100010
+        PUSH = 0b01000101
+        POP = 0b01000110 
 
         while not halted:
             instruction = self.ram_read(self.pc)
@@ -103,15 +101,36 @@ class CPU:
             if instruction == HLT:
                 halted = True
             elif instruction == LDI:
-                reg_num = self.ram[self.pc + 1]
+                reg_index = self.ram[self.pc + 1]
                 value = self.ram[self.pc + 2]
-                self.reg[reg_num] = value
-                self.pc += 3
+                self.reg[reg_index] = value
+                op_size = 3
             elif instruction == PRN:
-                reg_num = self.ram[self.pc + 1]
-                print(self.reg[reg_num])
-                self.pc += 2
+                reg_index = self.ram[self.pc + 1]
+                print(self.reg[reg_index])
+                op_size = 2
+            elif instruction == MUL:
+                register_a = self.ram[self.pc + 1]
+                register_b = self.ram[self.pc + 2]
+                self.alu("MUL", register_a, register_b)
+                op_size = 3
+            elif instruction == PUSH:
+                reg_index = self.ram[self.pc + 1]
+                val = self.reg[reg_index]
+                self.reg[SP] -= 1
+                self.ram[self.reg[SP]] = val
+                op_size = 2
+                print(f'Pushing {val} at index {reg_index} and decrementing SP to {self.reg[SP]}')
+            elif instruction == POP:
+                print(f'Popping off {val} at index {reg_index} and incrementing SP to {self.reg[SP]}')
+                reg_index = self.ram[self.pc + 1]
+                val = self.reg[SP]
+                self.reg[reg_index] = val
+                self.reg[SP] += 1
+                op_size = 2
             else:
                 print('unknown instruction')
+            
+            self.pc += op_size
 
-
+        print(f'REGISTERS: {self.reg}')
